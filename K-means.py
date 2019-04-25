de46e83d01daa943c665c05b74dc3e2f9205c652
Diana Gao <47991207+DianaGao@users.noreply.github.com>
2019-04-24 22:06:43 -0400

    Update K-means.py

--- K-means.py
+++ K-means.py
@@ -27,3 +27,20 @@ plt.xlabel('No. clusters')
 plt.ylabel('WCRR')
 plt.show()
 
+#train the k-means clustering model
+model = KMeans(n_clusters = 5, init = "k-means++", random_state = 0)
+y = model.fit_predict(x)
+
+# visualize the clusters
+plt.scatter(x[y == 0, 0], x[y == 0 , 1], s = 100, c = 'red', label = 'Oyster' )
+plt.scatter(x[y == 1, 0], x[y == 1 , 1], s = 100, c = "Orange", label = "Bread n butter" )
+plt.scatter(x[y == 2, 0], x[y == 2 , 1], s = 100, c = "Pink", label = "Perla" )
+plt.scatter(x[y == 3, 0], x[y == 3 , 1], s = 100, c = "Blue", label = "careless" )
+plt.scatter(x[y == 4, 0], x[y == 4 , 1], s = 100, c = "Brown", label = "sensible" )
+plt.scatter(model.cluster_centers_[:, 0],model.cluster_centers_[:,1], s = 50, c = 'black', label = 'centroid')
+plt.title('Customer types model')
+plt.xlabel('Annual income (K$)')
+plt.ylabel('Spending score (1-100)')
+plt.legend()
+plt.show()
+
